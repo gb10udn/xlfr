48b4c87ccebfefa0954219d110e3c17d1856838d
gb10udn <rbbpx0926@gmail.com>
2024-07-17 21:09:05 +0900

    add function to call from python (xlfr)

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,30 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use calamine::{Reader, open_workbook, Xlsx};
+use pyo3::prelude::*;
+
+#[pyfunction]
+fn read_excel(file_path: &str) -> PyResult<Vec<Vec<String>>> {
+    let mut workbook: Xlsx<_> = open_workbook(file_path).map_err(|e| {
+        pyo3::exceptions::PyIOError::new_err(format!("Failed to open workbook: {}", e))
+    })?;
+    let mut result = Vec::new();
+
+    if let Ok(sheet) = workbook.worksheet_range("Sheet1") {  // TODO: 240717 シート名を指定可能にせよ。
+        for row in sheet.rows() {
+            let mut row_data = Vec::new();  // TODO: 240717 skiprows のようなものを付けよ。
+            for cell in row {
+                row_data.push(cell.to_string());
+            }
+            result.push(row_data);
+        }
+    } else {
+        return Err(pyo3::exceptions::PyIOError::new_err("Sheet not found"));
+    }
+
+    Ok(result)
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[pymodule]
+fn xlfr(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(read_excel, m)?)?;
+    Ok(())
 }
